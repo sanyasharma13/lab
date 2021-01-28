9b5bfde5c17f0d40cd2ef1fd29133c4fa515aec1
Sanya <sanyasharma1316@gmail.com>
2021-01-27 20:36:42 -0600

    palindrome

---
+++ lab/lab00.py
@@ -0,0 +1,15 @@
+def my_palindrome():
+    return 'civic'
+
+# Do not change the main method
+def main():
+    pal = my_palindrome()
+    assert isinstance(pal, str), 'Must return a string'
+    assert pal.strip(), 'String should not be empty'
+    assert len(pal) > 1, 'String should be at least 2 letters long'
+    assert pal == pal[::-1], 'String is not a palindrome'
+    print("your successfully created a palindrome!")
+
+if __name__ == '__main__':
+    main()
+    
